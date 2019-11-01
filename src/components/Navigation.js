55e8f9ae1a111bfbe30021c6b2cfaf7cbcae8965
Eva <pazos.eva@gmail.com>
2019-11-01 20:49:37 +0100

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render(){
+        return (
+            <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white">
+                    Tasks:
+                    <span className="badge badge-pill badge-light ml-2">
+                    { this.props.tasks }
+                    </span>
+                </a>
+            </nav>
+        )
+    }
+}
+
+export default Navigation;
